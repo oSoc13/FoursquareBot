7e42387dd9672e98f5b85d00678247540baa54fb
Nik Torfs <niktorfs@gmail.com>
2013-07-02 11:42:48 +0200

    small utility too get current OS

---
+++ src/sample/OSChecker.java
@@ -0,0 +1,31 @@
+package sample;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nik
+ * Date: 02/07/13
+ * Time: 11:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class OSChecker {
+
+        public static String getOs(){
+            return System.getProperty("os.name").toLowerCase();
+        }
+
+        public static boolean isWindows() {
+            return (getOs().contains("win"));
+        }
+
+        public static boolean isMac() {
+            return (getOs().contains("mac"));
+        }
+
+        public static boolean isUnix() {
+            return (getOs().contains("nix") || getOs().contains("nux") || getOs().contains("aix"));
+        }
+
+        public static boolean isSolaris() {
+            return (getOs().contains("sunos"));
+        }
+}
