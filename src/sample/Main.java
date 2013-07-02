c3e6d90f5dafc98b326186de61f37c27c27d3b06
Linsey <linsey@vaio.(none)>
2013-07-02 15:13:47 +0200

    User authentication

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
+import fi.foyt.foursquare.api.FoursquareApiException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Vector;
 
 public class Main extends Application {
 
@@ -19,5 +22,26 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        FoursquareAPI fsAPI = new FoursquareAPI();
+
+        // Authenticate user
+        String code = "DPJ45L0LTEJ0BUKLJAEVKEEO3PWC55JIYJX5HGODR35HKQEH";
+        fsAPI.authenticateClient();
+        fsAPI.submitAccessCode(code);
+
+        // Search for venues
+        try {
+            Vector<String> venues = fsAPI.getNearbyVenues("50.951983,5.348959");
+            if (null != venues) {
+                fsAPI.checkInAt("Mobile Vikings");
+            }
+            else {
+            }
+        } catch (FoursquareApiException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
